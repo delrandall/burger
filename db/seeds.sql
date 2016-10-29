2c1641b2dd1fae996bad2478b9fe487d355c9054
delrandall <del_randall@hotmail.com>
2016-10-29 18:56:11 -0500

    Add files via upload

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Mushroom Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Jalapeno Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Salmon Burger', true);
+
+SELECT * FROM burgers;
